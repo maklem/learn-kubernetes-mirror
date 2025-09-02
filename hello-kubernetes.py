1f0d550ccb0d09641d479fdc858d60303e3c7088
Dr. Marco Klement <marco-klement-git@leuchtstark.dev>
2025-09-02 09:18:34 +0200

    adds: use storage for visitor counter

--- hello-kubernetes.py
+++ hello-kubernetes.py
@@ -1,10 +1,21 @@
 from flask import Flask
 
 app = Flask(__name__)
+COUNTER_FILENAME = "/data/counter"
 
 @app.route("/")
 def hello():
-    return "Hello, Kubernetes on Raspberry Pi 5 from Python!"
+    visitor_count = 0
+    with open(COUNTER_FILENAME, 'r') as f:
+        try: 
+            visitor_count = int(f.read())
+        except ValueError:
+            visitor_count = 0
+    visitor_count += 1
+    with open(COUNTER_FILENAME, 'w') as f:
+        f.write(f"{visitor_count}")
+
+    return f"Hello, Kubernetes on Raspberry Pi 5 from Python! You are visitor {visitor_count}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
